07436fef4e9d825b0d7befc82dfeb6cf83f9b821
mmahalwy <mohamed@peek.com>
2015-07-27 15:05:59 -0700

    Changes

--- blueprints/ember-cli-date-picker/index.js
+++ blueprints/ember-cli-date-picker/index.js
@@ -1,7 +1,9 @@
 module.exports = {
+  normalizeEntityName: function() {},
+
   afterInstall: function() {
     return this.addBowerPackageToProject('pikaday').then(function() {
-        return this.addBowerPackageToProject('moment');
+      return this.addBowerPackageToProject('moment');
     }.bind(this));
   }
 };
